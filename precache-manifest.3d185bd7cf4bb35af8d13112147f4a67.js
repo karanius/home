d65fdc86ff05458fd1150e710783c5498cf70970
Kavian Darvish <kavian1991@gmail.com>
2020-02-10 16:48:39 -0500

    Updates

---
+++ precache-manifest.3d185bd7cf4bb35af8d13112147f4a67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d331271a6689db9e787587216f20d728",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "4aa6789bbbaf61f37bdf",
+    "url": "/home/static/css/main.26d02e6c.chunk.css"
+  },
+  {
+    "revision": "4745687407ab9f6d7daa",
+    "url": "/home/static/js/2.0bdace17.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/home/static/js/2.0bdace17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa6789bbbaf61f37bdf",
+    "url": "/home/static/js/main.406370b8.chunk.js"
+  },
+  {
+    "revision": "7a8579e1e14f235afe91",
+    "url": "/home/static/js/runtime-main.f0341360.js"
+  },
+  {
+    "revision": "0a4b285b6e2828645fa4ad30e6b04352",
+    "url": "/home/static/media/character.0a4b285b.png"
+  },
+  {
+    "revision": "04fe26e5efdcac1d430ca7dd2de1562f",
+    "url": "/home/static/media/shadow.04fe26e5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b006a2950ca6af684e0fbbf67e30238e.js"
+  "/home/precache-manifest.3d185bd7cf4bb35af8d13112147f4a67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
