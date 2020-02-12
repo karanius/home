48267703f91469dcf865134a86d536178edbbabc
Kavian Darvish <kavian1991@gmail.com>
2020-02-12 16:26:05 -0500

    Updates

---
+++ precache-manifest.9cca5e151745e2e5afebe0a599358a57.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d18fc0414a61014071dcc8e1352407",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "03d2b2e82f3381ddf6b3",
+    "url": "/home/static/css/main.2f6fd8ee.chunk.css"
+  },
+  {
+    "revision": "e8502b05592e6b885cf9",
+    "url": "/home/static/js/2.27660c07.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/home/static/js/2.27660c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d2b2e82f3381ddf6b3",
+    "url": "/home/static/js/main.55d8b504.chunk.js"
+  },
+  {
+    "revision": "7a8579e1e14f235afe91",
+    "url": "/home/static/js/runtime-main.f0341360.js"
+  },
+  {
+    "revision": "0a4b285b6e2828645fa4ad30e6b04352",
+    "url": "/home/static/media/character.0a4b285b.png"
+  },
+  {
+    "revision": "0b123230d27f364f4962503c33aab416",
+    "url": "/home/static/media/gear.0b123230.svg"
+  },
+  {
+    "revision": "c2c478b48f7d0ca90ec9d2adf7e03992",
+    "url": "/home/static/media/gear2.c2c478b4.svg"
+  },
+  {
+    "revision": "04fe26e5efdcac1d430ca7dd2de1562f",
+    "url": "/home/static/media/shadow.04fe26e5.png"
+  }
+]);
