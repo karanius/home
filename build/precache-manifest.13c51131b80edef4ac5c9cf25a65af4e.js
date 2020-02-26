86050554daa9cf1e205e3521e67e9d3a504ffac1
Kavian Darvish <kavian1991@gmail.com>
2020-02-26 15:45:27 -0500

    final version one deploy

---
+++ build/precache-manifest.13c51131b80edef4ac5c9cf25a65af4e.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7e764eea8db059ddd0377c804f7bf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8833e16a34a1b4f765d",
+    "url": "/static/css/main.018bf0c2.chunk.css"
+  },
+  {
+    "revision": "6bb2df8ea36233c70cb9",
+    "url": "/static/js/2.5a38b5f1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.5a38b5f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8833e16a34a1b4f765d",
+    "url": "/static/js/main.caa45c2c.chunk.js"
+  },
+  {
+    "revision": "d372f04cfc35e0d3368a",
+    "url": "/static/js/runtime-main.bbe3537b.js"
+  },
+  {
+    "revision": "699e0c70da82af7541df61b6a3f091d8",
+    "url": "/static/media/bootstrap.699e0c70.svg"
+  },
+  {
+    "revision": "ead797223030d9f257d54626c4fa1295",
+    "url": "/static/media/character.ead79722.png"
+  },
+  {
+    "revision": "544d6b5aa7c849719f429c67434dd2fa",
+    "url": "/static/media/css3.544d6b5a.svg"
+  },
+  {
+    "revision": "1ea9bfeb867d115947ec67b993612624",
+    "url": "/static/media/devkavian.1ea9bfeb.jpeg"
+  },
+  {
+    "revision": "b319de81543dc3eb2cb68e6a6d5746b5",
+    "url": "/static/media/email.b319de81.svg"
+  },
+  {
+    "revision": "0b123230d27f364f4962503c33aab416",
+    "url": "/static/media/gear.0b123230.svg"
+  },
+  {
+    "revision": "c2c478b48f7d0ca90ec9d2adf7e03992",
+    "url": "/static/media/gear2.c2c478b4.svg"
+  },
+  {
+    "revision": "98a54548f14e58b534fd6a25dbb86492",
+    "url": "/static/media/git.98a54548.svg"
+  },
+  {
+    "revision": "793770168a4556985854b57f9061b285",
+    "url": "/static/media/github.79377016.svg"
+  },
+  {
+    "revision": "1a202c4f7bbf886b5fe0d0d3f11b6449",
+    "url": "/static/media/html5.1a202c4f.svg"
+  },
+  {
+    "revision": "7e04a69960e21a3d049c5807d94676b6",
+    "url": "/static/media/jquery.7e04a699.svg"
+  },
+  {
+    "revision": "c84f592a7d6ab49a59d62f262647d149",
+    "url": "/static/media/js.c84f592a.svg"
+  },
+  {
+    "revision": "1bdc2cede36a5fd3fb5834307fd72500",
+    "url": "/static/media/kd2020.1bdc2ced.pdf"
+  },
+  {
+    "revision": "44d22273ccf706817985a5cef80c7042",
+    "url": "/static/media/linkedin.44d22273.svg"
+  },
+  {
+    "revision": "e2988ed54229e8f4e6353b4ca05a1c50",
+    "url": "/static/media/mongo.e2988ed5.svg"
+  },
+  {
+    "revision": "04279c503ec1498497e3f43465047845",
+    "url": "/static/media/mysql.04279c50.svg"
+  },
+  {
+    "revision": "de843389ec3607b01af57d0561a1da03",
+    "url": "/static/media/node.de843389.svg"
+  },
+  {
+    "revision": "d0107bb21bf6d031c95328946511c0bf",
+    "url": "/static/media/photoshop.d0107bb2.svg"
+  },
+  {
+    "revision": "c037c57eb0914cbbba15ec1459a0fb17",
+    "url": "/static/media/react.c037c57e.svg"
+  },
+  {
+    "revision": "01ca5f3c23ebf2d88b9e701e59344245",
+    "url": "/static/media/resume.01ca5f3c.svg"
+  },
+  {
+    "revision": "d3e94e5a59f8f8bda97f1d19a64ea8eb",
+    "url": "/static/media/sass.d3e94e5a.svg"
+  },
+  {
+    "revision": "404ca5b5e1faca5a6bcb7a737d004fca",
+    "url": "/static/media/tie.404ca5b5.svg"
+  }
+]);
