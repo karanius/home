2633118291dd3acc44dc60bb8f7ad57a5eb8e35b
Kavian Darvish <kavian1991@gmail.com>
2020-03-09 14:59:38 -0400

    bug fixtest

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -13,6 +13,7 @@ const Card = ({id,techStack, about, title,imageLink,liveLink,repo}) => {
 
     const scrollAdjuster = (e) => {
       document.querySelector(`.card-top-front-${id}`).style.pointerEvents = "all";
+      document.querySelector(`.card-top-back-${id}`).style.pointerEvents = "all";
     }
 
     document.querySelector(`.card-top-front-${id}`).addEventListener("mouseover", scrollAdjuster);
