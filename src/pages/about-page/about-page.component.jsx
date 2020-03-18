2bc8cd8c8f19cb3e58c17e94bdf862be2e8a3665
Kavian Darvish <kavian1991@gmail.com>
2020-03-18 11:29:25 -0400

    update

--- src/pages/about-page/about-page.component.jsx
+++ src/pages/about-page/about-page.component.jsx
@@ -40,19 +40,15 @@ const AboutPage = () => {
               I was 5 years old when I started using a PC at home: Windows 95. I was obsessed. At the same time in our classes in middle school, they were teaching us "Logo" (<a href="https://en.wikipedia.org/wiki/Logo_(programming_language)">programming language</a>). And this is how I formed a special bond with electronics, computer programs and internet. 
             </p>
             <p className="p-of-msgs" >
-              My first attempt at coding was in 2015. I tought myself python and wrote a program to recieve and responsd to mass amounts of sms messages using a LTE SIM Dongle. 
+              My real first attempt at coding was in 2015. I tought myself python and wrote a program to recieve and responsd to mass amounts of sms messages using a LTE SIM Dongle. 
             </p>
             <p className="p-of-msgs" >
               I have been teaching myself how to code ever sense. Finally, in order to fill my knowledge gap, I decided to take the Full-Stack Web Development Bootcamp course at Uinversity of Toronto. 
             </p>
 
             <p className="p-of-msgs" >
-              Arts and science. A combo I can not get enough of. I have a Digital Film degree from Center for Arts and Technology (da Vinci School). Years of experience in digital arts, specially photo manipulation and coding has given me the creative skill present in my work.
+              Arts and science. A combo I can not get enough of. I have a Digital Film degree from Center for Arts and Technology (da Vinci School). Years of experience in digital arts, specially photo manipulation and coding has given me the creative skill set needed for this industry.
             </p>
-
-            {/* <p className="p-of-msgs" >
-              Currently looking to work with highly experienced Full-Stack, Front-End or Back-End enginners. 
-            </p> */}
 
           </div>
 
