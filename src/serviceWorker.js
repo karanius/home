0ceb2f052c9c5a75612e519032bbacb4e9f48b78
Kavian Darvish <kavian1991@gmail.com>
2020-03-27 16:21:20 -0400

    test

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,6 +21,7 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
+
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -30,6 +31,8 @@ export function register(config) {
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
       return;
     }
+    
+
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
