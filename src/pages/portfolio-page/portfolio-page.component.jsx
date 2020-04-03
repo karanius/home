e1181d771577c5591b12edab10c2ccd2f5a40a4a
Kavian Darvish <kavian1991@gmail.com>
2020-04-03 14:21:26 -0400

    auto-upload

--- src/pages/portfolio-page/portfolio-page.component.jsx
+++ src/pages/portfolio-page/portfolio-page.component.jsx
@@ -7,10 +7,6 @@ import Footer from '../../components/footer/footer.component';
 // import PortfolioSection from '../../components/portfolio-section/portfolio-section.component';
 
 
-
-
-
-
 const PortfolioPage = () => {
 
   useEffect(()=>{
