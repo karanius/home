67c2c91b119b126ff8f5efafb5950669069f3470
Kavian Darvish <kavian1991@gmail.com>
2020-04-03 14:17:28 -0400

    auto-upload

--- src/pages/portfolio-page/portfolio-page.component.jsx
+++ src/pages/portfolio-page/portfolio-page.component.jsx
@@ -4,7 +4,32 @@ import './portfolio-page.styles.scss'
 import TopMargin from '../../components/top-margin/top-margin-component';
 import BottomMargin from '../../components/bottom-margin/bottom-marign.components';
 import Footer from '../../components/footer/footer.component';
-import PortfolioSection from '../../components/portfolio-section/portfolio-section.component';
+// import PortfolioSection from '../../components/portfolio-section/portfolio-section.component';
+
+import Loadable from 'react-loadable';
+
+
+
+
+const Loading = () => {
+  return (
+    <div className="loader-bg">
+      <p className="loader-txt" >Downloading and loading assets</p>
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+
+// ********* this is the Lodable section
+  const PortfolioSection = Loadable({
+    loader: () => import('../../components/portfolio-section/portfolio-section.component'),
+    loading: Loading,
+  });
+
+
+
+
 
 const PortfolioPage = () => {
 
